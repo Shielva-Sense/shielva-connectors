c7260b8e1174cce789d331409c644e61196190cc
Shielva <vivek.sinha@shielva.ai>
2026-06-20 11:40:07 +0530

    [Shielva CI] SRQ_vivek_sinha_20260620_061004_36b45f5e

---
+++ generated_connectors/Tenant-f9184cb7/Confluence/__init__.py
@@ -0,0 +1,4 @@
+from .connector import ConfluenceConnector
+from .models import AuthStatus, ConnectorHealth, SyncStatus
+
+__all__ = ["AuthStatus", "ConfluenceConnector", "ConnectorHealth", "SyncStatus"]
---
+++ generated_connectors/Tenant-f9184cb7/Confluence/client/__init__.py
@@ -0,0 +1,3 @@
+from .http_client import ConfluenceHTTPClient
+
+__all__ = ["ConfluenceHTTPClient"]
