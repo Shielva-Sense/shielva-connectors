88c4943d2d7d51e0365723f47e8e536666570449
Shielva <vivek.sinha@shielva.ai>
2026-06-20 11:38:18 +0530

    [Shielva CI] SRQ_vivek_sinha_20260620_060551_5349795c (#126)

---
+++ generated_connectors/Tenant-f9184cb7/PayPal/__init__.py
@@ -0,0 +1,4 @@
+from .connector import PayPalConnector
+from .models import AuthStatus, ConnectorHealth, SyncStatus
+
+__all__ = ["AuthStatus", "ConnectorHealth", "PayPalConnector", "SyncStatus"]
---
+++ generated_connectors/Tenant-f9184cb7/PayPal/helpers/__init__.py
@@ -0,0 +1,3 @@
+from .utils import CircuitBreaker, normalize_order, normalize_transaction, with_retry
+
+__all__ = ["CircuitBreaker", "normalize_order", "normalize_transaction", "with_retry"]
